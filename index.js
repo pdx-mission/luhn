1a31017e55dd23cb7223d2ed4444910a79b119ba
Hunter Tunnicliff <hunter.tunnicliff@pdxmission.org>
2019-06-24 15:40:22 -0700

    Initialized repository

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Generates a check digit [0-9] based on string input
+ * @param {string} input
+ * @returns {number} a [0-9] integer
+ */
+module.exports = function luhnar(input) {
+  const validChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVYWXZ"
+  const cleanInput = input.toUpperCase().replace(/\s/g, "")
+
+  let sum = 0
+  for (var i = 0; i < cleanInput.length; i++) {
+    const ch = cleanInput.charAt(cleanInput.length - i - 1)
+    if (validChars.indexOf(ch) < 0) {
+      throw "Invalid character(s) found!"
+    }
+    const digit = ch.charCodeAt(0) - 48
+    let weight;
+    if (i % 2 == 0) {
+      weight = (2 * digit) - parseInt(digit / 5) * 9
+    } else {
+      weight = digit
+    }
+    sum += weight
+  }
+  sum = Math.abs(sum) + 10
+
+  const digit = (10 - (sum % 10)) % 10
+  return digit
+}
---
+++ index.spec.js
@@ -0,0 +1,50 @@
+const luhnar = require(".")
+
+const testCases = [
+  ["12", 5],
+  ["123", 0],
+  ["1245496594", 3],
+  ["TEST", 4],
+  ["Test123", 7],
+  ["00012", 5],
+  ["9", 1],
+  ["999", 3],
+  ["999999", 6],
+  ["CHECKDIGIT", 7],
+  ["EK8XO5V9T8", 2],
+  ["Y9IDV90NVK", 1],
+  ["RWRGBM8C5S", 5],
+  ["OBYY3LXR79", 5],
+  ["Z2N9Z3F0K3", 2],
+  ["ROBL3MPLSE", 9],
+  ["VQWEWFNY8U", 9],
+  ["45TPECUWKJ", 1],
+  ["6KWKDFD79A", 8],
+  ["HXNPKGY4EX", 3],
+  ["91BT", 2],
+  ["1018606 BB1 A GE", 8],
+  ["3 6TT A GE", 7],
+  ["3 743 A GE", 0],
+  ["3 791 A GE", 1],
+  ["3 793 A GE", 9],
+  ["3 842 A GE", 0],
+  ["3 891 A GE", 0],
+  ["7 711 A GE", 9],
+  ["7 793 A GE", 0],
+  ["7 842 A GE", 1],
+  ["7 913 A GE", 5],
+  ["9 6A3 A GE", 9],
+]
+
+describe.each(testCases)('luhnar "%s → %i"', (input, expected) => {
+  it("ignores whitespace", () => {
+    const regular = luhnar(input)
+    const noSpaces = luhnar(input.replace(/\s/g, ""))
+
+    expect(regular).toBe(noSpaces)
+  })
+
+  it("calculates expected digit", () => {
+    expect(luhnar(input)).toBe(expected)
+  })
+})
