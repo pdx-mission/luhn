365031fb0d281968ad30ca6dc982625111b12d93
Hunter Tunnicliff <hunter.tunnicliff@pdxmission.org>
2019-06-25 12:59:55 -0700

    Added comments

--- index.js
+++ index.js
@@ -5,27 +5,39 @@
  * @returns {number} a [0-9] integer
  */
 module.exports = function luhnar(input, validCharacters = /([0-9]|[A-Z])/) {
+  // Remove spaces and make all letters uppercase
   const cleanInput = input.toUpperCase().replace(/\s/g, "")
+
+  // Reverse the string
   const characters = cleanInput.split("").reverse()
 
+  // Initialize a sum of values
   let sum = 0
+
   for (const [idx, char] of characters.entries()) {
+    // Check validity of character
     if (!char.match(validCharacters)) {
       throw `Invalid character: "${char}". Does not match ${validCharacters}`
     }
 
-    let weight
+    // Store ASCII code for character, subtract 48
     const digit = char.charCodeAt(0) - 48
+
+    // Calculate weight based on even/odd index
+    let weight
     if (idx % 2 == 0) {
       weight = 2 * digit - Math.floor(digit / 5) * 9
     } else {
       weight = digit
     }
+
+    // Add weight to sum of values
     sum += weight
   }
 
+  // Calculate check digit
   sum = Math.abs(sum) + 10
-
   const digit = (10 - (sum % 10)) % 10
+
   return digit
 }
