a78a41a95feb3af4810e1e1b3cc4a41804deb0d5
Hunter Tunnicliff <hunter.tunnicliff@pdxmission.org>
2019-06-25 09:10:48 -0700

    Replaced for loop with for-of loop

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 /**
  * Generates a check digit [0-9] based on string input
  * @param {string} input
+ * @param {RegExp} [validCharacters]
  * @returns {number} a [0-9] integer
  */
-module.exports = function luhnar(input) {
-  const validChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVYWXZ"
+module.exports = function luhnar(input, validCharacters = /([0-9]|[A-Z])/) {
   const cleanInput = input.toUpperCase().replace(/\s/g, "")
+  const characters = cleanInput.split("").reverse()
 
   let sum = 0
-  for (var i = 0; i < cleanInput.length; i++) {
-    const ch = cleanInput.charAt(cleanInput.length - i - 1)
-    if (validChars.indexOf(ch) < 0) {
-      throw "Invalid character(s) found!"
+  for (const [idx, char] of characters.entries()) {
+    if (!char.match(validCharacters)) {
+      throw `Invalid character: "${char}". Does not match ${validCharacters}`
     }
-    const digit = ch.charCodeAt(0) - 48
-    let weight;
-    if (i % 2 == 0) {
-      weight = (2 * digit) - Math.floor(digit / 5) * 9
+
+    let weight
+    const digit = char.charCodeAt(0) - 48
+    if (idx % 2 == 0) {
+      weight = 2 * digit - Math.floor(digit / 5) * 9
     } else {
       weight = digit
     }
     sum += weight
   }
+
   sum = Math.abs(sum) + 10
 
   const digit = (10 - (sum % 10)) % 10
