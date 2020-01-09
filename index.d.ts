21046879b2007dd6aa0cf4517698c6435e47d649
Hunter Tunnicliff <hgtunnicliff@gmail.com>
2020-01-09 15:58:30 -0800

    Corrected typescript declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,9 @@
 declare module "@prm/luhn" {
-  export type CheckDigit = 0
-    | 1
-    | 2
-    | 3
-    | 4
-    | 5
-    | 6
-    | 7
-    | 8
-    | 9
+  export type CheckDigit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-  export function luhn(input: string, appendDigit = false, validCharacters: RegExp = /([0-9]|[A-Z])/): CheckDigit
+  export function luhn(
+    input: string,
+    appendDigit?: boolean,
+    validCharacters?: RegExp
+  ): CheckDigit;
 }
